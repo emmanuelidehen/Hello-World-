3d8cdfeaa4a9f3e939d6647bba7cba4347ae306b
Emmanuel idehen <emmanuel.idehen@mvsu.edu>
2019-03-14 17:46:38 -0500

    Add files via upload

---
+++ main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  Hello_world!
+//
+//  Created by Emmanuel Idehen on 3/14/19.
+//  Copyright © 2019 Emmanuel Idehen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+       // Hello world! 
+        NSLog(@"Hello, World!");
+        NSLog(@"\n This is Emmanuel Idehen first Objective C program!");
+        
+    }
+    return 0;
+}
